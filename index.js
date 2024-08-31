1c200066829f66a2817a7ff2307103fa407e1612
ashishstomar <23213257+ashishstomar@users.noreply.github.com>
2024-08-31 23:55:26 +0530

    feat(api): add URL shortener with MongoDB integration and visit tracking

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 const express = require("express");
-
+const urlRoute = require("./routes/url");
+const { connectMongoDB } = require("./connectdb");
+const URL = require("./models/url");
 const app = express();
 const PORT = 3000;
 
-app.get("/about", (req, res) => {
-  res.send("hello ");
+connectMongoDB("mongodb://localhost:27017/url-shortner").then(() =>
+  console.log("MongoDB connected")
+);
+
+app.use(express.json());
+
+app.use("/url", urlRoute);
+
+app.get("/:shortId", async (req, res) => {
+  const shortId = req.params.shortId;
+  const entry = await URL.findOneAndUpdate(
+    {
+      shortId,
+    },
+    {
+      $push: {
+        visitHistory: {
+          timestamp: Date.now(),
+        },
+      },
+    }
+  );
+  res.redirect(entry.redirectURL);
 });
-app.listen(PORT, () => `server started successfully at Port ${PORT}`);
+
+app.listen(PORT, () =>
+  console.log(`server started successfully at Port ${PORT}`)
+);
